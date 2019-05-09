f2f9a3ac646fcde83e3fcef56d092bb3aa3f3f98
0-Vector <tianchongqiang@gmail.com>
2019-05-09 10:06:15 +0800

    初始提交

---
+++ src/main/java/com/penny/xlab/XLabApplication.java
@@ -0,0 +1,13 @@
+package com.penny.xlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XLabApplication.class, args);
+	}
+
+}
